1da2eafbebf3ccb8ac124f042fb427ede5c3c9e8
paras231 <sharmaparas553@gmail.com>
2024-05-22 22:50:52 +0530

    add screenshot code

--- projects/learning.py
+++ projects/learning.py
@@ -1,2 +1,15 @@
 # learning content for python
 
+
+print("Learning")
+
+
+# take screenshot using python
+
+from PIL import ImageGrab
+
+# Capture the entire screen
+screenshot = ImageGrab.grab()
+
+# Save the screenshot to a file
+screenshot.save("screenshot.png")
